400d73e987756ccaf7e7ae59acb693a2a748e269
Kemal Skripic <kesk82@gmail.com>
2024-05-18 21:37:26 +0200

    Add fade in effect.

--- src/js/index.js
+++ src/js/index.js
@@ -31,3 +31,39 @@ if (toggleMobileNavigation) {
     }
   });
 }
+
+const showMoreBtn = document.querySelector('#intro .actions a');
+
+if (showMoreBtn) {
+  console.log("Found Show More BTN!");
+  showMoreBtn.addEventListener('click', e => {
+    e.preventDefault();
+    console.log("Click Show More BTN!!!");
+  });
+}
+
+const introContainer = document.getElementById('intro');
+
+function onPageScroll() {
+  if (window.scrollY > 50) {
+    if (introContainer) {
+      introContainer.classList.add('hidden');
+    }
+    if (toggleMobileNavigation) {
+      toggleMobileNavigation.classList.add('alt');
+    }
+  } else {
+    if (introContainer) {
+      introContainer.classList.remove('hidden');
+    }
+    if (toggleMobileNavigation) {
+      toggleMobileNavigation.classList.remove('alt');
+    }
+  }
+}
+
+document.addEventListener('scroll', onPageScroll);
+
+setTimeout(() => {
+  document.body.classList.remove('is-preload');
+}, 100);
